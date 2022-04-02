7e5a37b479d3e2c5758f6dc0e2cb5a57363b9fc3
gabikohen <gabokohen@gmail.com>
2022-04-01 23:16:33 -0300

    agrando vistas migraciones

---
+++ Aplicaciones/Academico/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+    codigo = models.CharField(primary_key=True,max_length=8)
+    name = models.CharField(max_length=50)
+    creditos = models.PositiveSmallIntegerField()
+
+""" Me deja mostrar el nombre del curso """
+
+def __str__(self):
+        text = "{0} ({1})"
+        return text.format(self.name, self.creditos)
