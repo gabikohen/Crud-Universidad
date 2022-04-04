176468cd9ed0d7926616190423add4e4ac0fe0ba
gabikohen <gabokohen@gmail.com>
2022-04-04 06:40:04 -0300

    crud termiando

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,37 +1,50 @@
 from django.shortcuts import render,redirect
 from .models import Curso
+from django.contrib import messages
 # Create your views here.
+
+""" Todos los cursos """
 
 def home(request):
     cursosListados = Curso.objects.all()
+    messages.success(request, '¡Cursos listados!')
     return render(request,"gestionCursos.html",{"cursos":cursosListados})
+
+
+""" Metodo para crear un curso """
 
 def registrarCurso(request):
  codigo = request.POST['txtCodigo']
  name = request.POST['txtNombre']
  creditos = request.POST['numCreditos']
 
- curso = Curso.objects.create(codigo=codigo, name=name,creditos=creditos)
+ curso = Curso.objects.create(codigo=codigo,name=name,creditos=creditos)
+ messages.success(request, '¡Curso registrado!')
  return redirect('/')
 
 
-def editCurso(request,codigo):
+""" Metodo para editar curso  """
+def editarCurso(request,codigo):
+ curso = Curso.objects.get(codigo=codigo)
+ return render(request,"editarCurso.html",{"curso":curso})
+
+def editCurso(request):
  codigo = request.POST['txtCodigo']
  name = request.POST['txtNombre']
  creditos = request.POST['numCreditos']
 
  curso = Curso.objects.get(codigo=codigo)
- curso.nombre = nombre
+ curso.name = name
  curso.creditos = creditos
  curso.save()
+ messages.success(request, '¡Curso actualizado!')
  return redirect('/')
 
 
-def editarCurso(request,codigo):
- curso = Curso.objects.get(codigo=codigo)
- return render(request,"editarCurso.html",{"curso":curso})
+""" Metodo Para eliminar un curso  """
 
 def deleteCurso(request,codigo):
  curso = Curso.objects.get(codigo=codigo)
  curso.delete()
+ messages.success(request, '¡Curso eliminado!')
  return redirect('/')
